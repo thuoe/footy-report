074e74fb744f65ff05267d0b5a19bd3cd0960777
Eddie Thuo <thuoeddie17@gmail.com>
2023-11-07 18:50:58 +0000

    feat: create dropdown with team sections using dummy data

--- src/search.tsx
+++ src/search.tsx
@@ -1,26 +1,58 @@
-import { Action, Icon, ActionPanel, List, LocalStorage, Color } from "@raycast/api";
+import { Action, Icon, ActionPanel, List, LocalStorage } from "@raycast/api";
 import { useCachedPromise } from "@raycast/utils";
 import { useEffect, useState } from "react";
+import TeamSections, { Category } from "@src/components/section";
 
-export default function Command() {
+type TeamListDropdownProps = {
+  categories: `${Category}`[];
+  onChange: (newValue: string) => void;
+};
+
+const TeamListDropdown = ({ categories, onChange }: TeamListDropdownProps) => {
+  return (
+    <List.Dropdown
+      storeValue
+      tooltip="Select Team Category"
+      onChange={onChange}
+      defaultValue="all"
+    >
+      {categories.map((category) => {
+        return (
+          <List.Dropdown.Item
+            title={category}
+            key={category}
+            value={category}
+          />
+        );
+      })}
+    </List.Dropdown>
+  );
+};
+
+export default () => {
   const { data, isLoading, revalidate } = useCachedPromise(async () => {
     return LocalStorage.getItem<string>("team-searches");
   });
   const [searchText, setSearchText] = useState("");
   const [team, selectTeam] = useState("");
+  const [category, setCategory] = useState<Category>(Category.All);
 
   const previousSearches: string[] = data ? JSON.parse(data) : [];
 
   useEffect(() => {
     const setList = async (list: string[]) => {
       LocalStorage.setItem("team-searches", JSON.stringify(list));
-      revalidate()
+      revalidate();
     };
     const updateList = async () => {
       const finalList = [...previousSearches, searchText];
       await setList(finalList);
     };
-    if (searchText.length > 3 && !isLoading && previousSearches.includes(searchText.trim()) === false) {
+    if (
+      searchText.length > 3 &&
+      !isLoading &&
+      previousSearches.includes(searchText.trim()) === false
+    ) {
       updateList().catch(console.error);
     }
   }, [searchText, isLoading]);
@@ -57,9 +89,13 @@ export default function Command() {
                           title="Clear Search"
                           icon={Icon.Trash}
                           onAction={async () => {
-                            console.debug("Action triggered!!");
-                            const newList = previousSearches.filter((search) => search !== item);
-                            LocalStorage.setItem("team-searches", JSON.stringify(newList));
+                            const newList = previousSearches.filter(
+                              (search) => search !== item,
+                            );
+                            LocalStorage.setItem(
+                              "team-searches",
+                              JSON.stringify(newList),
+                            );
                             revalidate();
                           }}
                         />
@@ -76,10 +112,17 @@ export default function Command() {
   }
 
   return (
-    <List>
-      <List.Section title="Current Match"></List.Section>
-      <List.Section title="Upcoming Fixtures"></List.Section>
-      <List.Section title="Squad"></List.Section>
+    <List
+      searchBarAccessory={
+        <TeamListDropdown
+          categories={Object.values(Category)}
+          onChange={(newValue) => {
+            setCategory(newValue as Category);
+          }}
+        />
+      }
+    >
+      <TeamSections category={category} />
     </List>
   );
-}
+};
