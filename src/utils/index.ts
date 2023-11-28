49bd7f0a2cca586bc1095f8456d6ed41b286e301
Eddie Thuo <thuoeddie17@gmail.com>
2023-11-28 23:55:29 +0000

    feat: create grouping function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,20 @@ export const computeSelectFields = <T extends Record<string, boolean>>(
   });
   return selectParams.length > 0 ? selectParams.join(",") : "";
 };
+
+export const groupBy = <T extends object, K extends keyof T>(
+  array: T[],
+  prop: K,
+): { [a in K]: T[] } => {
+  const map = new Map<T[K], T[]>();
+  array.forEach((element) => {
+    const groupName = element[prop];
+    const group = map.get(groupName);
+    if (!group) {
+      map.set(groupName, [element]);
+    } else {
+      map.set(groupName, [...group, element]);
+    }
+  });
+  return Object.fromEntries(map);
+};
