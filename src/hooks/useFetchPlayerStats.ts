a8e89547c88ac652de6403f0ea5572d4b74686c8
Eddie Thuo <thuoeddie17@gmail.com>
2023-12-12 15:57:06 +0000

    refactor: set/filter events

--- src/hooks/useFetchPlayerStats.ts
+++ src/hooks/useFetchPlayerStats.ts
@@ -1,5 +1,27 @@
 import useSportMonksClient from "./useSportMonksClient";
 
+enum EventType {
+  GOALS = 52,
+  ASSISTS = 79,
+  APPS = 321,
+}
+
+const isEvent =
+  (event: EventType) =>
+  ({ type_id }: { type_id: number }) => {
+    return type_id === event;
+  };
+
+const formatEvents = () => {
+  const values: number[] = [];
+  Object.keys(EventType)
+    .filter((v) => !isNaN(Number(v)))
+    .forEach((value) => {
+      values.push(Number(value));
+    });
+  return values.join(",");
+};
+
 const useFetchPlayerStats = ({
   id,
   teamId,
@@ -9,17 +31,15 @@ const useFetchPlayerStats = ({
 }) => {
   const { data, isLoading, revalidate } = useSportMonksClient({
     method: "get",
-    path: `/players/${id}?include=statistics.season;statistics.details&filters=playerStatisticDetailTypes:52,79,321;team=${teamId}`,
+    path: `/players/${id}?include=statistics.season;statistics.details&filters=playerStatisticDetailTypes:${formatEvents()};team=${teamId}`,
   });
 
   const stats = data?.data?.statistics.map(({ season, details }) => {
     const { name } = season;
-    const goals =
-      details.find(({ type_id }) => type_id === 52)?.value?.total ?? 0;
-    const assists =
-      details.find(({ type_id }) => type_id === 79)?.value?.total ?? 0;
+    const goals = details.find(isEvent(EventType.GOALS))?.value?.total ?? 0;
+    const assists = details.find(isEvent(EventType.ASSISTS))?.value?.total ?? 0;
     const appearances =
-      details.find(({ type_id }) => type_id === 321)?.value?.total ?? 0;
+      details.find(isEvent(EventType.APPS))?.value?.total ?? 0;
     return [name, goals, assists, appearances];
   });
 
