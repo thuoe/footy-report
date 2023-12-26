6d96886ddfb926f41f63636a0d7efbcca78eb4fd
Eddie Thuo <thuoeddie17@gmail.com>
2023-12-26 18:20:42 +0000

    feat: use form to test API access token

--- src/test.tsx
+++ src/test.tsx
@@ -1,65 +1,62 @@
-import {
-  Action,
-  ActionPanel,
-  Detail,
-  openExtensionPreferences,
-} from "@raycast/api";
-import { useSportMonksClient } from "@src/hooks";
+import { Action, ActionPanel, Form, Icon, useNavigation } from "@raycast/api";
+import { showFailureToast } from "@raycast/utils";
+import axios from "axios";
+import { useState } from "react";
+import { useAPIKey } from "@src/hooks";
+import APIResult from "@src/components/APIResult";
 
-const testPath = "/teams/search/Esbjerg";
-
-export default function Command() {
-  const { isLoading, data } = useSportMonksClient({
-    method: "get",
-    path: testPath,
-  });
-  const title =
-    data?.status === 200 ? "Successful Request!" : "Request Failure!";
-  const message =
-    data?.status === 200
-      ? "Great! You can now begin using commands!"
-      : "Whoops! There is a problem with your API Key!";
-  const imgSrc = data?.status == 200 ? "success" : "failure";
-  const image = `![](../assets/${imgSrc}.png)`;
-  const markdown = `
-  # ${title}
-  ${message}
-  ${image}
-  `;
-
+export default () => {
+  const { push } = useNavigation();
+  const apiKey = useAPIKey();
+  const [nameError, setNameError] = useState<string | undefined>();
+  const dropNameErrorIfNeeded = () => {
+    if (nameError && nameError.length > 0) {
+      setNameError(undefined);
+    }
+  };
   return (
-    <>
-      <Detail
-        isLoading={isLoading}
-        navigationTitle="API Key Test"
-        markdown={markdown}
-        actions={
-          <ActionPanel>
-            <Action
-              title="Open Extension Preferences"
-              onAction={openExtensionPreferences}
-            />
-          </ActionPanel>
-        }
-        metadata={
-          <Detail.Metadata>
-            <Detail.Metadata.Label title="Base URL" text={data?.baseURL} />
-            <Detail.Metadata.Label title="Test Path" text={testPath} />
-            <Detail.Metadata.TagList title="Content Type">
-              <Detail.Metadata.TagList.Item
-                text={data?.headers["content-type"]}
-                color="#B55ABE"
-              />
-            </Detail.Metadata.TagList>
-            <Detail.Metadata.TagList title="Status">
-              <Detail.Metadata.TagList.Item
-                text={data?.status?.toString()}
-                color={data?.status === 200 ? "#71C6B1" : "#D5562E"}
-              />
-            </Detail.Metadata.TagList>
-          </Detail.Metadata>
-        }
+    <Form
+      navigationTitle="Test SportMonks API"
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm
+            title="Send Request"
+            icon={Icon.Rocket}
+            onSubmit={async (values) => {
+              try {
+                const { url } = values;
+                const response = await axios({
+                  method: "GET",
+                  url,
+                  headers: { Authorization: apiKey },
+                });
+                push(<APIResult response={response} url={url} />);
+              } catch (error) {
+                showFailureToast(error);
+              }
+            }}
+          />
+        </ActionPanel>
+      }
+    >
+      <Form.Description
+        title="API Test"
+        text="To test the validity of your access token, make an API call using a SportMonks API endpoint (V3). For documentation on endpoints please visit https://docs.sportmonks.com/football/welcome/getting-started"
       />
-    </>
+      <Form.TextField
+        id="url"
+        title="URL"
+        placeholder="https://api.sportmonks.com/v3/some_path_here"
+        error={nameError}
+        onChange={dropNameErrorIfNeeded}
+        onBlur={(event) => {
+          if (event.target.value?.length === 0) {
+            setNameError("URL should not be empty!");
+          } else {
+            dropNameErrorIfNeeded();
+          }
+        }}
+      />
+    </Form>
   );
-}
+};
